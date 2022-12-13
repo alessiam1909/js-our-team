765657cd9b0aae6a7e40cb0c22a9fa50827e5dec
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-13 17:43:40 +0100

    milestone 2, stampo il contenuto nel dom

--- js/script.js
+++ js/script.js
@@ -1,38 +1,56 @@
- let members = [
+let contenuto = document.getElementById('contenuto');
+let persona, div, member;
+
+
+function creaElemento(){
+    div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `<p>${member.nome}<br>${member.ruolo}</p> <img src="./img/${member.img}">`;
+    return div;
+};
+
+
+let members = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        img: "./img/wayne-barnett-founder-ceo.jpg",
+        img: "wayne-barnett-founder-ceo.jpg",
     },
     {
         nome: "Angela caroll",
         ruolo: "Chied Editor",
-        img: "./img/angela-caroll-chief-editor.jpg",
+        img: "angela-caroll-chief-editor.jpg",
     },
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        img: "./img/walter-gordon-office-manager.jpg",
+        img: "walter-gordon-office-manager.jpg",
     },
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        img: "./img/angela-lopez-social-media-manager.jpg",
+        img: "angela-lopez-social-media-manager.jpg",
     },
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        img: "./img/scott-estrada-developer.jpg",
+        img: "scott-estrada-developer.jpg",
     },
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        img: "./img/barbara-ramos-graphic-designer.jpg",
+        img: "barbara-ramos-graphic-designer.jpg",
     }
  ]
 
- console.log(members);
+     
+for(let i = 0; i < members.length; i++){
+    member = members[i];
+    console.log(member);
+    creaElemento(member);
+    contenuto.appendChild(div);
 
- for(let key in members){
-    console.log(members[key]);
- }
+}
+    
+
+
