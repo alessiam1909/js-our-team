8a6d9c460217a693e4a41d388776f02c66a100bb
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-13 16:25:14 +0100

    creazione dell'array con informazioni fornite

---
+++ js/script.js
@@ -0,0 +1,34 @@
+ let members = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        img: "./img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela caroll",
+        ruolo: "Chied Editor",
+        img: "./img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        img: "./img/walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        img: "./img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        img: "./img/scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        img: "./img/barbara-ramos-graphic-designer.jpg",
+    }
+ ]
+
+ console.log(members);
